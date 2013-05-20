e38d3384617f662bfb3b42ce5b3689372236a95c
Paul Duncan <paul.shauna@gmail.com>
2013-05-19 22:29:10 -0700

    Add strength stat. Add greeting message depending on how strong you are.

--- JavaThing/src/whatsthis/Hero.java
+++ JavaThing/src/whatsthis/Hero.java
@@ -1,41 +1,81 @@
 package whatsthis;
 
-public class Hero{
-	
-	   private String name;
-	   private int health;
-	   private int mana;
-	   
-	   public Hero(String name, int health, int mana){
-	      //System.out.println("Hero Construction Begins! The power... IS YOURS");
-	      this.name = name;
-	      this.health = health;
-	      this.mana = mana;
-	   }
-	   
-	   public void mailCheck(){
-	      System.out.println("Mailing a check to " + this.name
-	       + " " + this.health);
-	   }
-	   
-	   public String toString(){
-	      return name + " " + health + " " + mana;
-	   }
-	   
-	   public String getName(){
-	      return name;
-	   }
-	   
-	   public int getHealth(){
-	      return health;
-	   }
-	   
-	   public void setHealth(int newHealth){
-		   health = newHealth;
-	   }
-	   
-	   public int getMana(){
-	     return mana;
-	   }
-	   
+public class Hero {
+
+	private String name;
+	private int health;
+	private int mana;
+	private int strength;
+
+	public Hero(String name, int health, int mana, int strength) {
+		// System.out.println("Hero Construction Begins! The power... IS YOURS");
+		this.name = name;
+		this.health = health;
+		this.mana = mana;
+		this.strength = strength;
+	}
+
+	public void mailCheck() {
+		System.out.println("Mailing a check to " + this.name + " "
+				+ this.health);
+	}
+
+	public String toString() {
+		return name + " " + health + " " + mana;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getGreatness() {
+		if (strength < 10) {
+			return "tiny.";
+		} else if (strength == 10) {
+			return "petite.";
+		} else if (strength == 11) {
+			return "small.";
+		} else if (strength == 12) {
+			return "average.";
+		} else {
+			return "healthy.";
+		}
+	}
+
+	/*
+	 * Lets set some methods to be able to get out heros current health and to
+	 * set it to a new number if needed..
+	 */
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int newHealth) {
+		health = newHealth;
+	}
+
+	/*
+	 * Lets set some methods to be able to get out heros current mana and to set
+	 * it to a new number if needed..
+	 */
+	public int getMana() {
+		return mana;
+	}
+
+	public void setMana(int newHealth) {
+		health = newHealth;
+	}
+
+	/*
+	 * Lets set some methods to be able to get out heros current strength and to
+	 * set it to a new number if needed..
+	 */
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int newStrength) {
+		strength = newStrength;
+	}
+
 }
--- JavaThing/src/whatsthis/Noidea.java
+++ JavaThing/src/whatsthis/Noidea.java
@@ -3,74 +3,84 @@ package whatsthis;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Noidea{
-	
+//import java.lang.Byte;
+
+public class Noidea {
+
 	private static Scanner user_input;
 
-	public static void main(String[] args)
-	{
-		//System.out.println("Git is awesome");
-		Randnum rn 			= new Randnum();
-		Scanner user_input 	= new Scanner(System.in);
-		
-		System.out.println("Adventurer! \n What grandiose days are these that a call to arms hath come across thine days. " +
-				"\n To venture forth upon the wildlands and meet many a strange new creature; and murder them. " +
-				"\n Pray tell, what is thine name?");
+	public static void main(String[] args) {
+		// System.out.println("Git is awesome");
+		Randnum rn = new Randnum();
+		Scanner user_input = new Scanner(System.in);
+
+		System.out
+				.println("Adventurer! \n What grandiose days are these that a call to arms hath come across thine days. "
+						+ "\n To venture forth upon the wildlands and meet many a strange new creature; and murder them. "
+						+ "\n Pray tell, what is thine name?");
 		String hn;
 		hn = user_input.next();
-		
-		System.out.println("Dost thou hast a preferred school of murderous arts, "+hn+"? Mage or perchance Warrior?");
-		
+
+		System.out
+				.println("Dost thou, having thine name of "
+						+ hn
+						+ ", hast a preferred school of murderous arts? Mage or perchance Warrior?");
+
 		String jobz;
 		jobz = user_input.next();
+
 		int hl;
 		int mg;
-		
-		if (jobz.equals("Mage") || jobz.equals("mage")){
+		int st;
+		String lol;
+		if (jobz.equals("Mage") || jobz.equals("mage")) {
 			hl = 35;
 			mg = 100;
-		}else{
+			st = 10;
+			lol = "magerrific";
+		} else {
 			hl = 85;
-		    mg = 10;
+			mg = 10;
+			st = 11;
+			lol = "warriorlicious";
 		}
-		
-		Hero herro = new Hero(hn, hl, mg);
+
+		Hero herro = new Hero(hn, hl, mg, st);
 		int health = herro.getHealth();
 		int mana = herro.getMana();
+		int strength = herro.getStrength();
+		String gr8 = herro.getGreatness();
 		String myheroname = herro.getName();
-		System.out.println(myheroname+". A good name. A name they shall rise their glass of mead to in a heros song!");
+
+		// System.out
+		// .println(myheroname
+		// +
+		// ". A good name. A name they shall rise their glass of mead to in a heros song!");
 		System.out.println("----------------------------------");
-		System.out.println("Hello, "+myheroname+".");
-		System.out.println("You have: "+ health+" health and "+mana+" mana.");
+		System.out.println("Greetings, " + myheroname + ". The "+gr8);
+		System.out.println("You have: " + health + " health and " + mana
+				+ " mana. Your strength is level: " + strength);
 		System.out.println("----------------------------------");
-		
-		//System.out.println("----------------------------------");
-		//System.out.println("Hello, "+myheroname+".");
-		//System.out.println("You have: "+ health+" health and "+mana+" mana.");
-		//System.out.println("----------------------------------");
-		//int health = 50;
-		/*
-		while( health>0 )
-		{	
-			//System.out.println(rndNum);
-			//System.out.println(rndNum);
+
+		while (health > 0) {
+			// System.out.println(rndNum);
+			// System.out.println(rndNum);
 			int ii = rn.getRandNum();
-			
-			//System.out.println(health);
+
+			// System.out.println(health);
 			System.out.println("Attack? Y/N");
 			String attack;
 			attack = user_input.next();
-			
-			if (attack.equals("Y") || attack.equals("y")){
+
+			if (attack.equals("Y") || attack.equals("y")) {
 				System.out.println("KILL!");
 				System.out.println("You've done " + ii + " damage!");
 				health = health - ii;
 				System.out.println(health);
-			
 			} else {
 				System.out.println("cower..");
 			}
 		}
-		System.out.println("!!!-------------------->DEATH!!!");*/
+		System.out.println("!!!-------------------->DEATH!!!");
 	}
 }
